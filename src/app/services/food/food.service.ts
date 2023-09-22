807e80fa791d3c0f4a2405f8e0aeba63cf58e762
Chetana-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-09-22 14:01:20 +0530

    food service

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,5 +1,7 @@
+import { R3BoundTarget } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Food } from 'src/app/model/food.model';
+import { Tag } from 'src/app/model/tags.model';
 import { sample_foods } from 'src/data';
 
 @Injectable({
@@ -11,5 +13,30 @@ export class FoodService {
 
   getAll():Food[]{
     return sample_foods ; 
+  }
+
+  getSearchedFood(searchItem : string){
+    return this.getAll().filter(food => food.name?.toLowerCase().includes(searchItem.toLowerCase()))
+  }
+
+  getAllByTag(tag : string):Tag[]{
+    return tag == 'All' ? this.getAll() : this.getAll().filter(food => food.tags?.includes(tag))
+  }
+
+  getAllTag():Tag[]{
+    return [
+      {name : 'All' , count : 8},
+      {name : 'fastfood' , count : 4},
+      {name : 'pizza' , count : 3},
+      {name : 'lunch' , count : 3},
+      {name : 'slowfood' , count : 2},
+      {name : 'hamburger' , count : 2},
+      {name : 'fry' , count : 1},
+      {name : 'soup' , count : 1},
+  ]
+  }
+
+  getFoodbyID(foodID : string){
+    return this.getAll().find(food => food.id === foodID) ?? new Food() ; 
   }
 }
